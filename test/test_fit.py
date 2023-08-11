49bc778e203d010c2c55db5c14fff900213ae6bd
Nick <129078463+nicholasjmorris1993@users.noreply.github.com>
2023-08-11 17:18:26 -0400

    Add files via upload

--- test/test_fit.py
+++ test/test_fit.py
@@ -1,1 +1,11 @@
+import pandas as pd
+import sys
+sys.path.append("/home/nick/Distribution-Identification/src")
+from fit import test
 
+
+data = pd.read_csv("/home/nick/Distribution-Identification/test/LungCap.csv")
+
+distribution = test(data["LungCap"].to_numpy())
+
+distribution.summary
