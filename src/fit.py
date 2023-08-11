2200db505a7063bd8a6ea6f31450f6a795db5480
Nick <129078463+nicholasjmorris1993@users.noreply.github.com>
2023-08-11 17:17:40 -0400

    Add files via upload

--- src/fit.py
+++ src/fit.py
@@ -1,1 +1,23 @@
+import numpy as np
+import pandas as pd
+from distfit import distfit
+
+
+def test(x):
+    model = Test()
+    model.testing(x)
+    model.plot()
+
+    return model
+
+
+class Test:
+    def testing(self, x):
+        self.dist = distfit()
+        self.dist.fit_transform(x)
+
+        self.summary = self.dist.summary
+
+    def plot(self):
+        self.dist.plot()
 
